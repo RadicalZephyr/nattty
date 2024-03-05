27a59944d87e1b594695c4acd14ad60e51201c33
Zefira Shannon <earthlingzephyr@gmail.com>
2024-03-05 11:45:25 -0800

    Move main fn to the top of the file

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,24 @@ mod board;
 
 use board::{Board, Mark};
 
+fn main() {
+    let ctx = SodiumCtx::new();
+
+    let (kb_input, _listeners) = ctx.transaction(|| {
+        let mut listeners = Vec::new();
+        let kb_input: StreamSink<String> = ctx.new_stream_sink();
+
+        set_up_play(&kb_input, &mut listeners, &ctx);
+
+        (kb_input, listeners)
+    });
+
+    let stdin = std::io::stdin().lock();
+    for line in stdin.lines() {
+        kb_input.send(line.unwrap());
+    }
+}
+
 fn set_up_play(
     kb_input: &StreamSink<String>,
     listeners: &mut Vec<sodium::Listener>,
@@ -89,22 +107,4 @@ fn mark_swapping(ctx: &SodiumCtx, index_stream: &Stream<usize>) -> Cell<Mark> {
         turn_cell_loop.loop_(&turn_cell);
         turn_cell
     })
-}
-
-fn main() {
-    let ctx = SodiumCtx::new();
-
-    let (kb_input, _listeners) = ctx.transaction(|| {
-        let mut listeners = Vec::new();
-        let kb_input: StreamSink<String> = ctx.new_stream_sink();
-
-        set_up_play(&kb_input, &mut listeners, &ctx);
-
-        (kb_input, listeners)
-    });
-
-    let stdin = std::io::stdin().lock();
-    for line in stdin.lines() {
-        kb_input.send(line.unwrap());
-    }
 }
