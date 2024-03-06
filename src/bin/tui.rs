8cdac089ff0b726b9bdfa116306e70b5e1e2e014
Zefira Shannon <earthlingzephyr@gmail.com>
2024-03-05 21:55:54 -0800

    Make it slightly smaller but still proportional to the window size

--- src/bin/tui.rs
+++ src/bin/tui.rs
@@ -83,49 +83,55 @@ impl Ui {
 
     fn draw_board<B: Backend>(&self, f: &mut Frame<B>, area: Rect) {
         let thirds = [
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(1, 3),
+            Constraint::Ratio(1, 5),
+            Constraint::Ratio(1, 5),
+            Constraint::Ratio(1, 5),
+            Constraint::Ratio(1, 5),
+            Constraint::Ratio(1, 5),
         ];
         let vchunks = Layout::default()
             .direction(Direction::Vertical)
             .margin(0)
             .constraints(thirds.as_ref())
             .split(area);
+
         let hchunks0 = Layout::default()
             .direction(Direction::Horizontal)
             .margin(0)
+            .horizontal_margin(3)
             .constraints(thirds.as_ref())
-            .split(vchunks[0]);
+            .split(vchunks[1]);
         let block = Block::default().borders(Borders::RIGHT | Borders::BOTTOM);
-        f.render_widget(block, hchunks0[0]);
-        let block = Block::default().borders(Borders::ALL ^ Borders::TOP);
         f.render_widget(block, hchunks0[1]);
-        let block = Block::default().borders(Borders::LEFT | Borders::BOTTOM);
+        let block = Block::default().borders(Borders::ALL ^ Borders::TOP);
         f.render_widget(block, hchunks0[2]);
+        let block = Block::default().borders(Borders::LEFT | Borders::BOTTOM);
+        f.render_widget(block, hchunks0[3]);
 
         let hchunks1 = Layout::default()
             .direction(Direction::Horizontal)
             .margin(0)
+            .horizontal_margin(3)
             .constraints(thirds.as_ref())
-            .split(vchunks[1]);
+            .split(vchunks[2]);
         let block = Block::default().borders(Borders::ALL ^ Borders::LEFT);
-        f.render_widget(block, hchunks1[0]);
-        let block = Block::default().borders(Borders::ALL);
         f.render_widget(block, hchunks1[1]);
-        let block = Block::default().borders(Borders::ALL ^ Borders::RIGHT);
+        let block = Block::default().borders(Borders::ALL);
         f.render_widget(block, hchunks1[2]);
+        let block = Block::default().borders(Borders::ALL ^ Borders::RIGHT);
+        f.render_widget(block, hchunks1[3]);
 
         let hchunks2 = Layout::default()
             .direction(Direction::Horizontal)
             .margin(0)
+            .horizontal_margin(3)
             .constraints(thirds.as_ref())
-            .split(vchunks[2]);
+            .split(vchunks[3]);
         let block = Block::default().borders(Borders::RIGHT | Borders::TOP);
-        f.render_widget(block, hchunks2[0]);
-        let block = Block::default().borders(Borders::ALL ^ Borders::BOTTOM);
         f.render_widget(block, hchunks2[1]);
-        let block = Block::default().borders(Borders::LEFT | Borders::TOP);
+        let block = Block::default().borders(Borders::ALL ^ Borders::BOTTOM);
         f.render_widget(block, hchunks2[2]);
+        let block = Block::default().borders(Borders::LEFT | Borders::TOP);
+        f.render_widget(block, hchunks2[3]);
     }
 }
